3dee9e422cae6c0709fdbfa3bfb26ba011f99ed8
amelin <g-amelin@mail.ru>
2022-04-17 17:00:14 +0300

    added checkTotal test

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -1,9 +1,23 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
 public class SelenoidTests {
 
     // make request to https://selenoid.autotests.cloud/status
     // total is 20
 
+    @Test
+    void checkTotal() {
 
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+
+    }
 }

ba83f9a84abe0b15544e6a594035ec8d18ea6d41
amelin <g-amelin@mail.ru>
2022-04-19 09:08:06 +0300

    added TestBase and BeforeEach annotation

--- src/test/java/tests/ReqresinTests.java
+++ src/test/java/tests/ReqresinTests.java
@@ -1,6 +1,8 @@
 package tests;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,34 +13,32 @@ import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ReqresinTests {
+public class ReqresinTests extends TestBase{
+
+    @BeforeEach
+    public void precondition() {
+        RestAssured.baseURI = "https://reqres.in/api";
+    }
 
     @Test
     @DisplayName("Создание нового пользователя (внутренние проверки)")
     void createNewUserCheck() {
 
-//        request: https://reqres.in/api/users
-
-//        data: {"name": "gregorrr", "job": "QA"}
-
-//        response: {"name": "gregorrr", "job": "QA", "id": "912", "createdAt": "2022-04-18T15:00:24.175Z"}
-
-        String registrationData = "{ \"name\": \"gregorrr\", \"job\": \"QA\" }";
+        String registrationData = "{ \"name\": \"" + name + "\", \"job\": \"" + firstJob + "\" }";
 
         given()
                 .body(registrationData)
                 .contentType(JSON)
                 .when()
-                .post("https://reqres.in/api/users")
+                .post("/users")
                 .then()
                 .statusCode(201)
-                .body("name", is("gregorrr"))
-                .body("job", is("QA"))
+                .body("name", is(name))
+                .body("job", is(firstJob))
                 .body("", hasKey("id"))
                 .body("", hasKey("createdAt"));
     }
@@ -47,19 +47,19 @@ public class ReqresinTests {
     @DisplayName("Создание нового пользователя (внешние проверки)")
     void createNewUserWithOutsideCheck() {
 
-        String registrationData = "{ \"name\": \"gregorrr\", \"job\": \"QA\" }";
+        String registrationData = "{ \"name\": \"" + name + "\", \"job\": \"" + firstJob + "\" }";
 
         Response response = given()
                 .body(registrationData)
                 .contentType(JSON)
                 .when()
-                .post("https://reqres.in/api/users")
+                .post("/users")
                 .then()
                 .extract().response();
 
         assertEquals(response.getStatusCode(), 201);
-        assertEquals(response.path("name"), "gregorrr");
-        assertEquals(response.path("job"), "QA");
+        assertEquals(response.path("name"), name);
+        assertEquals(response.path("job"), firstJob);
         assertTrue(response.getBody().asString().contains("id"));
         assertTrue(response.getBody().asString().contains("createdAt"));
 
@@ -73,19 +73,19 @@ public class ReqresinTests {
     @DisplayName("Обновление данных пользователя (внешние проверки)")
     void updateNewUserWithOutsideCheck() {
 
-        String registrationData = "{ \"name\": \"gregorrr\", \"job\": \"PHD\" }";
+        String registrationData = "{ \"name\": \"" + name + "\", \"job\": \"" + secondJob + "\" }";
 
         Response response = given()
                 .body(registrationData)
                 .contentType(JSON)
                 .when()
-                .put("https://reqres.in/api/users/60")
+                .put("/users/60")
                 .then()
                 .extract().response();
 
         assertEquals(response.getStatusCode(), 200);
-        assertEquals(response.path("name"), "gregorrr");
-        assertEquals(response.path("job"), "PHD");
+        assertEquals(response.path("name"), name);
+        assertEquals(response.path("job"), secondJob);
         assertTrue(response.getBody().asString().contains("updatedAt"));
 
         System.out.println("\n" + "Response: " + response.asString());
@@ -107,7 +107,7 @@ public class ReqresinTests {
 
         Response response = given()
                 .when()
-                .get("https://reqres.in/api/users?page=" + pageNumber)
+                .get("/users?page=" + pageNumber)
                 .then()
                 .extract().response();
 
@@ -127,7 +127,7 @@ public class ReqresinTests {
 
         given()
                 .when()
-                .get("https://reqres.in/api/users?page=2")
+                .get("/users?page=2")
                 .then()
                 .statusCode(200)
                 .body("page", is(2))
@@ -145,9 +145,9 @@ public class ReqresinTests {
                 .when()
                 .body(userCredentials)
                 .contentType(JSON)
-                .post("https://reqres.in/api/login")
+                .post("/login")
                 .then()
                 .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X9"));
+                .body("token", is(notNullValue()));
     }
 }
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBase {
+
+    public static final String name = "gregorrr";
+    public static final String firstJob = "QA";
+    public static final String secondJob = "PHD";
+
+}

2c398e19a4374cdc40a82b4e128b8d2717a90866
amelin <g-amelin@mail.ru>
2022-04-24 00:37:16 +0300

    modified DemowebshopTests

--- src/test/java/tests/DemowebshopTests.java
+++ src/test/java/tests/DemowebshopTests.java
@@ -3,12 +3,12 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 public class DemowebshopTests {
     @Test
     void addToCardTest() {
-        String response =
-                given()
+        given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .cookie("Nop.customer=4a270881-47e7-42c9-a179-438c242fa564; " +
                         "ARRAffinity=1818b4c81d905377ced20e7ae987703a674897394db6e97dc1316168f754a687")
@@ -19,8 +19,11 @@ public class DemowebshopTests {
                 .when()
                 .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
                 .then()
-                .extract().response().asString();
-        System.out.println(response);
-
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your " +
+                        "\u003ca href=\"/cart\"\u003eshopping cart\u003c/a\u003e"))
+                .body("updatetopcartsectionhtml", is("(8)"));
     }
 }

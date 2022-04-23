90a38473d2546274d4de49cfe9fb0012c0e4ea09
amelin <g-amelin@mail.ru>
2022-04-23 23:42:56 +0300

    modified DemowebshopTests

--- src/test/java/tests/DemowebshopTests.java
+++ src/test/java/tests/DemowebshopTests.java
@@ -15,7 +15,7 @@ public class DemowebshopTests {
                 .body("product_attribute_72_5_18=53" +
                         "&product_attribute_72_6_19=54" +
                         "&product_attribute_72_3_20=57" +
-                        "&addtocart_72.EnteredQuantity=1")
+                        "&addtocart_72.EnteredQuantity=")
                 .when()
                 .post("http://demowebshop.tricentis.com/addproducttocart/details/72/1")
                 .then()
